1751a6558a8934315374db261ad1dcc94fe23775
long <nguyenthanhlong@morenergy.net>
2023-08-24 15:44:05 +0200

    refactor: delete component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LinePlotComponent } from './components/line-plot/line-plot/line-plot.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LinePlotComponent
   ],
   imports: [
     BrowserModule,
