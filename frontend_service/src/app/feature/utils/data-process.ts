f9beaa22e8c8fd58bb6370cb75654202bcccbfed
long <nguyenthanhlong@morenergy.net>
2023-09-06 14:28:46 +0200

    feat: group by device function

--- frontend_service/src/app/feature/utils/data-process.ts
+++ frontend_service/src/app/feature/utils/data-process.ts
@@ -1,6 +1,18 @@
 import { Results } from "../usage-report/models/results";
 
-export function groupDataByDevice(data: Results[]) {
-
-    return
+export function groupDataByDevice(data: any[]) {
+    const groupedData = data.reduce((accumulator, item) => {
+        const deviceId = item.deviceId
+        if (!accumulator[deviceId]) {
+            accumulator[deviceId] = {
+                name: `Device ${deviceId}`,
+                type: 'line',
+                data: [],
+            }
+        }
+        accumulator[deviceId].data.push([item.timestamp, item.temperature])
+        return accumulator
+    }, {});
+    console.log(groupedData)
+    return groupedData
 }
